b1a6f37745d141a583b469122b9870dea6f99907
singerrkim <kym4824@hanyang.ac.kr>
2017-12-11 13:48:03 +0900

    placeholder 활용 방법

--- lab_01_02.py
+++ lab_01_02.py
@@ -1,14 +1,17 @@
 import tensorflow as tf
 
-x_train = [1, 2, 3]
-y_train = [1, 2, 3]
+#x_train = [1, 2, 3]
+#y_train = [1, 2, 3]
+
+X = tf.placeholder(tf.float32)
+Y = tf.placeholder(tf.float32)
 
 W = tf.Variable(tf.random_normal([1]), name='weight')
 b = tf.Variable(tf.random_normal([1]), name='bias')
 
-hypothesis = x_train * W + b
+hypothesis = X * W + b
 
-cost = tf.reduce_mean(tf.square(hypothesis - y_train))
+cost = tf.reduce_mean(tf.square(hypothesis - Y))
 
 optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
 train = optimizer.minimize(cost)
@@ -18,6 +21,6 @@ sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
 for step in range(2001):
-    sess.run(train)
-    if step % 20 == 0:
-        print(step, sess.run(cost), sess.run(W), sess.run(b))
+   cost_val, W_val, b_val, _ = sess.run([cost, W, b, train], feed_dict={X: [1, 2, 3], Y: [1, 2, 3]})
+   if step % 20 == 0:
+       print(step, cost_val, W_val, b_val)
