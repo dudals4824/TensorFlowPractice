68bcef368185973f35ed7addeac40e3d8882ea34
dudals4824 <dudals4824@gmail.com>
2017-12-11 15:13:16 +0900

    optional 강의 부분 주석으로 달아놓음

--- lab_03_03.py
+++ lab_03_03.py
@@ -14,6 +14,11 @@ train = optimizer.minimize(cost)
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
+## gvs = optimizer.compute_gradients(cost)
+## 컴퓨터가 계산해주는 gradient 식을 우리가 수정해서 쓸 수 있다.
+## apply_gradients = optimizer.apply_gradients(gvs)
+## sess.run(gvs) 해도 되는것 같음
+
 for step in range(100):
     print(step, sess.run(W))
     sess.run(train)
