da71913d13e53cf40d8bd890dc033ea3a6874af4
dudals4824 <dudals4824@gmail.com>
2017-12-18 14:48:08 +0900

    !동물의 종류를 맞추는 머신 python 표현 방법을 좀 알아두면 좋을 것 같다. 현실에서 쓰일 수 있는 가장 가까운 게 softmax 같다.

--- lab_06_02.py
+++ lab_06_02.py
@@ -27,12 +27,28 @@ cost_i = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=Y_one_hot
 cost = tf.reduce_mean(cost_i)
 optimizer = tf.train.GradientDescentOptimizer(learning_rate= 0.1).minimize(cost)
 
+# with tf.Session() as sess:
+#    sess.run(tf.global_variables_initializer())
+#
+#    for step in range(2001):
+#       cost_val, hy_val, _ = sess.run([cost, hypothesis, optimizer], feed_dict={X: x_data, Y: y_data})
+#       if step % 10 == 0:
+#           print(step, "Cost: ", cost_val, "\n Prediction: \n", hy_val)
+
+# 훈련시키는 부분 다른 표현 방법과 정확도 확인하는 부분
+
+prediction = tf.argmax(hypothesis, 1) # hypothesis의 확률 값을 label중 하나로 만들어준다.
+correct_prediction = tf.equal(prediction, tf.argmax(Y_one_hot, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
 
     for step in range(2001):
-       cost_val, hy_val, _ = sess.run([cost, hypothesis, optimizer], feed_dict={X: x_data, Y: y_data})
-       if step % 10 == 0:
-           print(step, "Cost: ", cost_val, "\n Prediction: \n", hy_val)
-
-
+        sess.run(optimizer, feed_dict={X: x_data, Y: y_data})
+        if step % 100 == 0:
+            loss, acc = sess.run([cost, accuracy], feed_dict = {X: x_data, Y:y_data})
+            print("Step : {:5} \t Loss : {:.3f} \t Acc: {:.2%}".format(step, loss, acc))
+    pred = sess.run(prediction, feed_dict={X: x_data})
+    for p, y in zip(pred, y_data.flatten()):
+        print("[{}] Prediction: {} True Y: {}".format(p == int(y), p, int(y)))
